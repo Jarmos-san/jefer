2e1a3c061ef6e9c2cf578af772e83185ede5111c
Somraj Saha <somraj.1994@gmail.com>
2022-09-15 11:35:39 +0530

    moved the project entrypoint file to an appropriate location

---
+++ jefer/main.py
@@ -0,0 +1,12 @@
+"""The main entrypoint file for Jefer."""
+
+
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def message() -> None:
+    """Print a "Hello World!" message."""
+    typer.echo("Hello World!")
