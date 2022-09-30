11b060c30599f2185bf45ca1cd4a99bdb71c8cd2
Somraj Saha <somraj.1994@gmail.com>
2022-09-30 19:43:14 +0530

    create some bare minimum functions to refactor later

--- jefer/main.py
+++ jefer/main.py
@@ -11,5 +11,41 @@ def message() -> None:
     typer.echo("Hello Jarmos!")
 
 
+@app.command()
+def init() -> None:
+    """Create an empty Git repository on the local system."""
+    pass
+
+
+@app.command()
+def remove() -> None:
+    """Remove a source file & its destination link from the system."""
+    pass
+
+
+@app.command()
+def link() -> None:
+    """Create a symbolic link for a source file."""
+    pass
+
+
+@app.command()
+def list() -> None:
+    """Show the list of files & the path to their destined links."""
+    pass
+
+
+@app.command()
+def verify() -> None:
+    """Check if a source file(s) has a linked destination."""
+    pass
+
+
+@app.command()
+def backup() -> None:
+    """Use Git to push the latest updates in the source file to a remote repository."""
+    pass
+
+
 if __name__ == "__main__":
     app()
